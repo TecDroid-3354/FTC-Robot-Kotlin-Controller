fe9413f64f9577be402354f293dbb72bbcf854c3
JuPalbo <a01713288@tec.mx>
2026-06-15 23:46:44 -0600

    Juan Pablo Ramírez. Kotlin FTC Robot Controller. Utils, Kotlin, Solverslib, Panels, Pedro and FTC Dashboard installed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/Intake.kt
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.subsystems.intake
-
-class Intake {
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/IntakeConstants.kt
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.subsystems.intake
-
-object IntakeConstants {
-}
