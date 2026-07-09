fe50a58938daa1c24b5f45a74c9628b6a028572f
JuPalbo <A01713288@tec.mx>
2026-07-09 11:18:51 -0600

    Juan Pablo Ramírez. Very simple code for this year's TecDroid Academy Robot Ñoverture. The robot consists of one tankdrive, a motor to intake and one to shoot. Nothing omre nothing less. Code located at Robot.kt, as previously said, its only purpose is testing, this way of writing code shsould not be repeatedly used. Check out this year's FTC Robot code, that actually represents the way we as a team write code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
@@ -1,18 +1,40 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.PIDCoefficients
 import com.seattlesolvers.solverslib.command.InstantCommand
 import com.seattlesolvers.solverslib.command.Robot
+import com.seattlesolvers.solverslib.command.RunCommand
+import com.seattlesolvers.solverslib.command.SubsystemBase
+import com.seattlesolvers.solverslib.command.button.Trigger
+import com.seattlesolvers.solverslib.controller.wpilibcontroller.SimpleMotorFeedforward
 import com.seattlesolvers.solverslib.gamepad.GamepadEx
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.teamcode.subsystems.SimpleMotorSubsystem
 import org.firstinspires.ftc.teamcode.utils.Alliance
+import org.firstinspires.ftc.teamcode.utils.AngularVelocity
 import org.firstinspires.ftc.teamcode.utils.TecDroidRobot
+import org.firstinspires.ftc.teamcode.utils.configurations.OpMotorExConfiguration
+import org.firstinspires.ftc.teamcode.utils.configurations.controlModeConfigurations.PercentageModeConfiguration
+import org.firstinspires.ftc.teamcode.utils.configurations.controlModeConfigurations.VelocityModeConfiguration
+import org.firstinspires.ftc.teamcode.utils.configurations.genericConfigurations.GenericMotorConfiguration
+import org.firstinspires.ftc.teamcode.utils.constants.RobotConstants.Telemetry.pTelemetry
+import org.firstinspires.ftc.teamcode.utils.devices.OpMotorEx
+import org.firstinspires.ftc.teamcode.utils.extensions.leftBumper
+import org.firstinspires.ftc.teamcode.utils.extensions.onFalse
+import org.firstinspires.ftc.teamcode.utils.extensions.onTrue
+import org.firstinspires.ftc.teamcode.utils.extensions.rightBumper
 
 class Robot(private val alliance: Alliance, private val hardwareMap: HardwareMap, private val controller: GamepadEx, telemetry: Telemetry): TecDroidRobot(telemetry, hardwareMap) {
 
     /* Declare your subsystems here */
-    private lateinit var sampleMotor: SimpleMotorSubsystem
+    private lateinit var shooterMotor: OpMotorEx
+
+    private lateinit var intakeMotor: OpMotorEx
+
+    private lateinit var rightMotor: OpMotorEx
+    private lateinit var leftMotor: OpMotorEx
 
     init {
         subsystemInitialization()
@@ -20,22 +42,42 @@ class Robot(private val alliance: Alliance, private val hardwareMap: HardwareMap
 
     /* Initialize your subsystems here */
     override fun subsystemInitialization() {
-        sampleMotor = SimpleMotorSubsystem(hardwareMap)
+        shooterMotor = OpMotorEx(hardwareMap, "sm")
+        shooterMotor.applyConfigurationAndResetEncoder(OpMotorExConfiguration(
+            GenericMotorConfiguration(),
+            VelocityModeConfiguration()
+                .withFeedforwardCoefficients(SimpleMotorFeedforward(0.0, 1.5, 0.0))
+                .withVelocityCoefficients(PIDCoefficients(0.1, 0.0, 0.0))
+        ))
+
+        intakeMotor = OpMotorEx(hardwareMap, "im")
+        intakeMotor.applyConfigurationAndResetEncoder(OpMotorExConfiguration(
+            GenericMotorConfiguration().withInverted(true),
+            PercentageModeConfiguration()
+        ))
+
+        rightMotor = OpMotorEx(hardwareMap, "rm")
+        rightMotor.applyConfigurationAndResetEncoder(OpMotorExConfiguration(
+            GenericMotorConfiguration().withInverted(true)
+        ))
+
+        leftMotor = OpMotorEx(hardwareMap, "lm")
+        leftMotor.applyConfigurationAndResetEncoder(OpMotorExConfiguration())
     }
 
     /* Initialize your teleop controller commands here */
     override fun initTeleOp() {
-        /* a button, turn on velocity mode */
-        controller
+        Trigger {controller.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.3}
+            .whileActiveOnce(InstantCommand({ shooterMotor.setVelocity(AngularVelocity(80.0)) }))
+            .whenInactive(InstantCommand({ shooterMotor.stopMotor() }))
 
-        /* b button, turn on position mode */
-        controller
+        controller.rightBumper()
+            .onTrue(InstantCommand({ intakeMotor.setPower(1.0) }))
+            .onFalse(InstantCommand({ intakeMotor.setPower(0.0) }))
 
-        /* y button, turn on raw power mode */
-        controller
-
-        /* turn on motor depending on mode */
-        controller
+        controller.leftBumper()
+            .onTrue(InstantCommand({ intakeMotor.setPower(-0.2) }))
+            .onFalse(InstantCommand({ intakeMotor.setPower(0.0) }))
     }
 
     /* Initialize your auto commands here, set chassis alliance and starting pose */
@@ -45,5 +87,14 @@ class Robot(private val alliance: Alliance, private val hardwareMap: HardwareMap
     override fun onEnd() {}
 
     // Print telemetry using the pTelemetry object on RobotConstants.Telemetry. It will be printed on both Panels and Driver Hub.
-    override fun updateTelemetry() {}
+    override fun printTelemetry() {
+        RunCommand({
+            rightMotor.setPower(-controller.leftY + controller.rightX)
+            leftMotor.setPower(-controller.leftY - controller.rightX)
+        }).schedule()
+        pTelemetry.addData("Motor set power", intakeMotor.getOutput())
+        pTelemetry.addData("Motor velocity", shooterMotor.getVelocity().get().rps)
+        pTelemetry.addData("Motor position", intakeMotor.getPosition().get().rotations)
+        pTelemetry.addData("Motor acceleration", shooterMotor.getAcceleration().get().rotPerSecPerSec)
+    }
 }
